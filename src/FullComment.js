c3f975edbb3e0c7dc87bef7b2de1c2c7be8ba36e
SepehrShapouri <110922785+SepehrShapouri@users.noreply.github.com>
2023-06-04 12:31:11 +0330

    fullcomment states added

--- src/FullComment.js
+++ src/FullComment.js
@@ -1,13 +1,23 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 const FullComment = ({ commentId }) => {
+  const [fullComment, setFullComment] = useState(null);
+  useEffect(() => {
+    if(commentId){
+      axios
+      .get(`https://jsonplaceholder.typicode.com/comments/${commentId}`)
+      .then(res => setFullComment(res.data))
+      .catch();
+    }
+  }, [commentId]);
+
   if (!commentId)
     return <p className="selectcomment">Please select a comment!</p>;
   return (
     <div className="fullcomment">
-      <p>name</p>
-      <p>email</p>
-      <p>body</p>
+      <p>{fullComment.name}</p>
+      <p>{fullComment.email}</p>
+      <p>{fullComment.body}</p>
     </div>
   );
 };
