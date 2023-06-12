6227096c2f0aa470fa27d7ef450861866afa2315
SepehrShapouri <110922785+SepehrShapouri@users.noreply.github.com>
2023-06-12 20:24:48 +0330

    axios post method developed and new comments can now be sent to backend database

--- src/components/NewComment.js
+++ src/components/NewComment.js
@@ -1,40 +1,39 @@
 import { useState } from "react";
-
+import axios from "axios";
 const NewComment = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [body, setBody] = useState("");
-  const submitHandler = () => {};
-  const changeHandler = () => {};
+  const [comment, setComment] = useState({
+    name: "",
+    email: "",
+    content: "",
+  });
+  const handleInputChange = (e) => {
+    const { id, value } = e.target;
+    setComment({ ...comment, [id]: value });
+  };
+  const submitHandler = (e) => {
+    e.preventDefault();
+    axios
+      .post("https://jsonplaceholder.typicode.com/comments", {
+        ...comment,
+        postId: 1,
+      })
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err));
+  };
   return (
     <section className="newcomment">
       <form onSubmit={submitHandler} className="commentform">
         <span>
           <label htmlFor="name">name</label>
-          <input
-            type="text"
-            defaultValue={name}
-            onChange={changeHandler}
-            id="name"
-          />
+          <input type="text" onChange={handleInputChange} id="name" />
         </span>
         <span>
           <label htmlFor="email">email</label>
-          <input
-            type="text"
-            defaultValue={email}
-            onChange={changeHandler}
-            id="email"
-          />
+          <input type="text" onChange={handleInputChange} id="email" />
         </span>
         <span>
           <label htmlFor="body">body</label>
-          <input
-            type="textarea"
-            defaultValue={body}
-            onChange={changeHandler}
-            id="body"
-          />
+          <input type="textarea" onChange={handleInputChange} id="body" />
         </span>
         <button className="addbtn">add</button>
       </form>
