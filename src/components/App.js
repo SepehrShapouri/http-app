7d762c91502b0457517a07a6d03397044ce66bcf
SepehrShapouri <110922785+SepehrShapouri@users.noreply.github.com>
2023-07-31 12:38:16 +0330

    app converted to spa using react router dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,20 @@
-import Discussion from "./Discussion";
+import Discussion from "./Comments";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Layout from "../layout/Layout";
-import { BrowserRouter, Route,Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage";
-import NewComment from "./NewComment";
+import NewComment from "../pages/NewComment";
+import FullComment from "../pages/FullComment";
 
 const App = () => {
   return (
     <BrowserRouter>
-       <Layout>
+      <Layout>
         <Routes>
-          <Route path="homepage" element={<HomePage/>}/>
-          <Route path="newcomment" element={<NewComment/>}/>
+          <Route path="homepage" element={<HomePage />} />
+          <Route path="newcomment" element={<NewComment />} />
+          <Route path="comment/:id" element={<FullComment />} />
         </Routes>
       </Layout>
     </BrowserRouter>
